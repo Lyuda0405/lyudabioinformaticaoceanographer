db2fd26bfd7c96362ea42f713e05e56d1b079638
Lyuda0405 <ludmillaveado@estudante.ufscar.br>
2024-05-13 12:00:30 -0300

    Add files via upload

---
+++ trabalhos/Atividade 1 Ludmilla.py
@@ -0,0 +1,25 @@
+1.	Verificador de palíndromo (criar um algoritmo para um palíndromo)
+Input: palíndromo
+Output: ‘True’
+1º inserir a palavra amor
+2º criar uma lista de palavras
+3º verificar se há o palíndromo roma
+4º retorna ‘True’
+
+Input ‘amor’
+Input List: [amor, roma]
+    If roma in List [] do:
+         For ‘roma’ == ‘amor’ do:
+                 ‘True’
+                  break
+    else:
+         For ‘roma’ == ‘roma’ 
+                 'False'
+                  break
+
+2.	Desafio diário: molhar a grama toda noite
+Input: horário do anoitecer
+Output: molhar a grama
+1º inserir o horário do anoitecer
+2º no horário que for após às 18h
+3º retornar: molhar a grama
